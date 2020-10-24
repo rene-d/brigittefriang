a817cdea6a6b11aa8f67285d27256ea00528994e
rene-d <rene-d@users.noreply.github.com>
2020-10-24 22:01:55 +0200

    Update getfiles.sh

--- etape2/forensic/getfiles.sh
+++ etape2/forensic/getfiles.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-
+wget -nc -nv https://www.challengecybersec.fr/chat/public/uploads/access.log
 wget -nc -nv https://www.challengecybersec.fr/chat/public/uploads/evil_country_landscape.jpg
